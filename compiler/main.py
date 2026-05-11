2805187f7cf58c342721c614554810f1f4591d39
umutstudio <baharelidenk@gmail.com>
2026-05-11 19:40:58 +0300

    Create main.py

---
+++ compiler/main.py
@@ -0,0 +1,14 @@
+from compiler.lexer import tokenize
+from compiler.parser import parse
+from compiler.codegen import generate
+from compiler.runtime import run
+
+from compiler import tokenize, parse, generate, run
+
+with open("examples/simple_game.yeni", "r", encoding="utf-8") as f:
+    code = f.read()
+
+tokens = tokenize(code)
+ast = parse(tokens)
+generated_code = generate(ast)
+run(generated_code)
